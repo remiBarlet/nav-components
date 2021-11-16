d67b9cfa20b999a11c59ecc2534414a1db94a9a4
remiBarlet <rbarlet@protonmail.com>
2021-11-16 16:03:52 +0100

    buttons/menuComponents directories added btns added

--- src/components/buttons/BurgerCenterToCross.js
+++ src/components/buttons/BurgerCenterToCross.js
@@ -4,7 +4,7 @@ import './BurgerCenterToCross.css'
 const BurgerCenterToCross = (props) => {
 
     return(
-        <div className={`burgerCenterToCross ${ props.isOpen ? 'open' : 'closed' }`} >
+        <div className={`burgerCenterToCross ${ props.isOpen ? 'open' : 'closed' }`}>
             <div className='burgerCenterToCross-lines-1'></div>
             <div className='burgerCenterToCross-lines-2'></div>
             <div className='burgerCenterToCross-lines-3'></div>
