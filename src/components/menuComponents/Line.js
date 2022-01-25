2e3da498229d337ee23f5be03163bc3c084299f9
remiBarlet <rbarlet@protonmail.com>
2022-01-25 15:35:14 +0100

    adding props to customize line component: color/bkColor/hoverColor/bkHoverColor/ fontSize/paddingLeft/ height

--- src/components/menuComponents/Line.js
+++ src/components/menuComponents/Line.js
@@ -4,9 +4,40 @@ import PropTypes from 'prop-types'
 
 const Line = (props) => {
 
+    //object to override standard color and background-color of the line, standard and hover
+    let style = {}
+    props.color && (style['--color'] = `${ props.color }`)
+    props.bkColor && (style['--bkColor']= `${ props.bkColor }`)
+    props.hoverColor && (style['--hoverColor'] = `${ props.hoverColor}`)
+    props.hoverBkColor && (style['--hoverBkColor'] = `${ props.hoverBkColor}`)
+    props.fontSize && (style['--fontSize'] = `${ props.fontSize}em`)
+    props.paddingLeft && (style['--paddingLeft']= `${ props.paddingLeft }em`)
+    props.height && (style['--height']= `${ props.height }em`)
+
+    //hook to set the childrens visibles
+    const [ isOpen, setIsOpen ] = useState(false)
+    const onClick = () => setIsOpen(!isOpen)
+
+    //function to pass the props to the non-Line childs 
+    const childrenWithPropsNoLines = React.Children.map(props.children, child =>
+        child.type !== Line && React.cloneElement(child, { onClick, isOpen })    
+    )
+    //function to pass the props to the Line childs 
+    const childrenWithPropsLines = React.Children.map(props.children, child =>
+        child.type === Line && React.cloneElement(child)    
+    )
+
     return(
-        <li className='line' id={ props.id }>
+        <li id={ props.id }> 
+            <div className='line'  style={ style } >
                 <div className='title'><a href={ props.href }>{ props.title }</a></div>
+                { childrenWithPropsNoLines }
+            </div>
+        { (isOpen || props.allwaysOpen) && (
+            <ul>
+                { childrenWithPropsLines }
+            </ul>
+        ) }
         </li>
     )
 
@@ -15,8 +46,13 @@ const Line = (props) => {
 
 Line.propTypes = {
     id: PropTypes.string, 
+    allwaysOpen: PropTypes.bool, 
     title: PropTypes.string,
-    href: PropTypes.string
+    href: PropTypes.string,
+    color: PropTypes.string,
+    bkColor: PropTypes.string,
+    hoverColor: PropTypes.string,
+    hoverBkColor: PropTypes.string
 }
 
 export default Line
